88dab028ceffb0e310fe50c316f80781c7b7f874
LikynAI <Likyn2001@gmail.com>
2018-09-27 22:59:37 +0300

    Второе задание.

---
+++ Program (2).cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtLs3_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int sum = 0;
+			int n;
+			while (true)
+			{
+				Console.Clear();
+				Console.WriteLine("Вводите числа");
+
+				string i = Console.ReadLine();
+				if (Int32.TryParse(i, out n))
+				{
+					if (n % 2 == 1)
+					{
+						sum += n;
+					}
+					else if (n == 0)
+					{
+						break;
+					}
+				}
+				else
+				{
+					Console.WriteLine(i + " нея вляется числом.");
+					Console.ReadLine();
+				}
+			}
+			Console.WriteLine(sum);
+			Console.ReadLine();
+		}
+	}
+}
