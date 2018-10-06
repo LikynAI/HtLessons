d5882ef24207433f1a67d475a0e9c3fc055e07f5
LikynAI <Likyn2001@gmail.com>
2018-10-06 13:53:06 +0300

    добавлена проверка регулярным выражением

--- PasswordCheker.cs
+++ PasswordCheker.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApp7
 {
 	class PasswordCheker
 	{
-		public static bool chek(string Password)
+		public static bool chek(string login)
 		{
-			StringBuilder p = new StringBuilder(Password);
+			StringBuilder p = new StringBuilder(login);
 			if (2 <= p.Length && p.Length <= 10)
 			{
 				for (int i = 0; i < p.Length; i++)
@@ -44,6 +45,12 @@ namespace ConsoleApp7
 				}
 			}
 			return true;
+		}
+
+		public static bool chek2(string login)
+		{
+			Regex regex = new Regex("^[a-zA-Zа-яА-Я0-9][a-zA-Zа-яА-Я0-9]{1,10}$");
+			return regex.IsMatch(login);
 		}
 	}
 }
